3f723a46bc0668cf1415f295ec2279767f317aec
SamLion <mailsonanacleto13@gmail.com>
2021-10-15 04:11:01 -0300

    dessa vez vai

---
+++ script/c112345681.lua
@@ -0,0 +1,5 @@
+-- JOJO Polnareff
+local s, id = GetID()
+function s.initial_effect(c)
+  -- effects
+end
---
+++ script/c112345682.lua
@@ -0,0 +1,5 @@
+-- DARK JOJO DIO
+local s, id = GetID()
+function s.initial_effect(c)
+  -- effects
+end
