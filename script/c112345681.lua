ff498a1c6d58ad5648dfc155411d4c2a6d8c4c1a
SamLion <mailsonanacleto13@gmail.com>
2025-11-09 20:05:35 -0300

    Update c112345681.lua
    Adicionado Script da carta JOJO Polnareff

--- script/c112345681.lua
+++ script/c112345681.lua
@@ -1,5 +1,27 @@
--- JOJO Polnareff
+--JOJO Polnareff
 local s, id = GetID()
 function s.initial_effect(c)
   -- effects
+  local e1=Effect.CreateEffect(c)
+  e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+  e1:SetType(EFFECT_TYPE_IGNITION)
+  e1:SetCode(EVENT_FREE_CHAIN)
+  e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+  e1:SetRange(LOCATION_MZONE)
+  e1:SetOperation(s.activate)
+  c:RegisterEffect(e1)
+end
+s.listed_series={0x0f99}
+function s.thfilter(c)
+  return (c:IsType(TYPE_SPELL) or c:IsType(TYPE_TRAP)) and c:IsSetCard(0x0f99) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+  if not e:GetHandler():IsRelateToEffect(e) then return end
+  local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+  if #g>0 then
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local sg=g:Select(tp,1,1,nil)
+    Duel.SendtoHand(sg,nil,REASON_EFFECT)
+    Duel.ConfirmCards(1-tp,sg)
+  end
 end
