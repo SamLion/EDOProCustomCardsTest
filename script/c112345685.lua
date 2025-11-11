8be3f7874cf5bb4495be004d3ce758437861d3b4
SamLion <mailsonanacleto13@gmail.com>
2025-11-11 15:47:15 -0300

    update script

--- script/c112345685.lua
+++ script/c112345685.lua
@@ -66,9 +66,49 @@ function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			e1:SetValue(function(e,c) return c==tc end)
 			c:RegisterEffect(e1)
+
+			local e2=Effect.CreateEffect(c)
+			e2:SetDescription(aux.Stringid(id,1))
+			e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+			e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+			e2:SetRange(LOCATION_MZONE)
+			e2:SetCondition(s.btlcon)
+			e2:SetOperation(s.btlop)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2)
 		end
 	else
 		-- Se falhar, envia ao cemitério
 		Duel.SendtoGrave(c,REASON_EFFECT)
 	end
+end
+
+function s.btlcon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=c:GetBattleTarget()
+    if not bc then return false end
+    local ec=c:GetEquipGroup():GetFirst()
+    return ec and ec:IsCode(id) -- ainda está equipado com esta carta
+end
+
+function s.btlop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local bc=c:GetBattleTarget()
+    if not bc or not c:IsRelateToBattle() then return end
+    -- Impede dano
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetTargetRange(1,1)
+    e1:SetValue(0)
+    e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+    Duel.RegisterEffect(e1,tp)
+    
+    -- Impede o monstro do oponente de atacar até a próxima End Phase do oponente
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_CANNOT_ATTACK)
+    e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+    bc:RegisterEffect(e2)
 end
