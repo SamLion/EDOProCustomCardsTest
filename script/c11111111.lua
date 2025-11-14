d403689440320ed87c8547e42c008f7d354cd48c
SamLion <mailsonanacleto13@gmail.com>
2025-11-14 15:23:31 -0300

    Update c11111111.lua

--- script/c11111111.lua
+++ script/c11111111.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
     --Ativar um dos efeitos
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
+    -- e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_COUNTER+CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_MZONE)
